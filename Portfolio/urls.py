2fa02ce35a00bcbe8584813d801533d84ee858f4
Bryium <bryiumonyancha@gmail.com>
2024-07-11 21:25:40 +0300

    fixed the errors

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,3 +1,4 @@
+# noinspection GrazieInspection
 """Portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -5,7 +6,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views. Home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -16,11 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-path("Home/", include("Home.urls"))
-
-
+    path('home/', include('home.urls')),  # Ensure your app name is 'home' and the path matches your folder structure
 ]
