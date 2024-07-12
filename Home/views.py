f4dbac747c5386ae68c4df796be20bd1611ad5b9
Bryium <bryiumonyancha@gmail.com>
2024-07-12 13:44:33 +0300

    fixed error in urls.py

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 
+
 def home(request):
     # Logic for rendering the home page
     template = loader.get_template('home.html')
--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),  # Ensure your app name is 'home' and the path matches your folder structure
+    path('Home/', include('Home.urls')),  # Ensure your app name is 'home' and the path matches your folder structure
 ]
