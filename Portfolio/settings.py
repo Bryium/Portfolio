4062bf031795f32323d7e9595ec930384421a0fc
Bryium <bryiumonyancha@gmail.com>
2024-09-09 15:59:12 +0300

    add requirment txtx file and postgres connection

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-c1*jf*2&*q5_aeoe==i1m9-1a9g9=65pkc!w!*ygykyf)rd8#c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
@@ -69,10 +69,21 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Portfolio',
+        'USER': 'postgres',
+        'PASSWORD': 'BryiuM8234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -106,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'Home/static',
 ]
